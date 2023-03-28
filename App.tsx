33f1b00609a4e1f832ad02d35960c4aed75a91c3
Rafinhaa <rafinhaa@me.com>
2023-03-28 12:49:36 -0300

    feat: Adiciona Styled Components

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {ThemeProvider} from 'styled-components';
+
+import theme from './src/global/styles/theme';
 
 const App = (): JSX.Element => {
   return (
-    <SafeAreaView>
-      <Text>Hello world!</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text>Hello world!</Text>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 };
 
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,22 @@
+export default {
+  colors: {
+    primary: '#00BFFF',
+    secondary: '#1E90FF',
+    background: '#0C2340',
+    backgroundLight: '#1d3557',
+    titleText: '#FFFFFC',
+    normalText: '#D3D3D3',
+    redLight: '#FF5733',
+    greenLight: '#5CB85C',
+  },
+  fontSizes: {
+    title: '22px',
+    subTitle: '18px',
+    text: '16px',
+  },
+  radii: {
+    sm: '2px',
+    md: '4px',
+    lg: '8px',
+  },
+};
