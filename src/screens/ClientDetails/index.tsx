2000d8f04809909b92de60d95661f82dc53a9078
Rafinhaa <rafinhaa@me.com>
2023-03-29 19:44:49 -0300

    feat: Tela base ClientDetails

---
+++ src/screens/ClientDetails/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import {BaseScreen} from '../BaseScreen';
+
+export const ClientDetails: React.FC = () => {
+  return (
+    <BaseScreen>
+      <Text>Hello!</Text>
+    </BaseScreen>
+  );
+};
