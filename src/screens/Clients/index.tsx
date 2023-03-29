7924f7de629de56b00b475d9be515f35f269c73f
Rafinhaa <rafinhaa@me.com>
2023-03-29 12:17:40 -0300

    feat: Cria tela base Clients

---
+++ src/screens/Clients/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Typographic} from '../../components/Typographic';
+
+import {BaseScreen} from '../BaseScreen';
+
+export const Clients: React.FC = () => {
+  return (
+    <BaseScreen>
+      <Typographic.Title>Clients</Typographic.Title>
+    </BaseScreen>
+  );
+};
