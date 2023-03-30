f4baaf9e4bebc90b27c88042be2634bc92e8fa98
Rafinhaa <rafinhaa@me.com>
2023-03-30 13:50:14 -0300

    feat: Implementa tela BankSlips no grupo de rotas

--- src/routes/app.stack.routes.tsx
+++ src/routes/app.stack.routes.tsx
@@ -3,6 +3,8 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {Clients} from '../screens/Clients';
 import {ClientDetails} from '../screens/ClientDetails';
+import {BankSlips} from '../screens/BankSlips';
+
 import {RootStackParamsList} from './types';
 
 const {Navigator, Screen, Group} =
@@ -17,6 +19,7 @@ export const ClientsStackRoutes = () => {
       <Group>
         <Screen name="Clients" component={Clients} />
         <Screen name="ClientDetails" component={ClientDetails} />
+        <Screen name="BankSlips" component={BankSlips} />
       </Group>
     </Navigator>
   );
--- src/routes/types.ts
+++ src/routes/types.ts
@@ -3,6 +3,7 @@ import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 export type RootStackParamsList = {
   Clients: undefined;
   ClientDetails: {id: string};
+  BankSlips: {id: string};
 };
 
 export type AppNavigationProps = NativeStackNavigationProp<RootStackParamsList>;
